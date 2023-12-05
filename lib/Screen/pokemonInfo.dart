b3b53bbb5a771013460e8bbfb2a9e8c612455374
7ELEVEN\jiratchsan <jiratchsan@gosoft.co.th>
2023-12-05 17:04:40 +0700

    update

--- lib/Screen/pokemonInfo.dart
+++ lib/Screen/pokemonInfo.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-
+import 'package:http/http.dart' as http;
 
 class PokemonInfo extends StatefulWidget {
   final String? username;
@@ -18,6 +17,45 @@ class PokemonInfoState extends State<PokemonInfo> {
   void initState() {
  
     super.initState();
+
+  }
+
+
+  Future<List<PokemonDetail>> fetchAllPokemonDetails() async {
+    try {
+      final response = await http.get(Uri.parse('https://pokeapi.co/api/v2/pokemon'));
+
+      if (response.statusCode == 200) {
+        final List<dynamic> results = json.decode(response.body)['results'];
+        List<Pokemon> pokemons = results.map((json) => Pokemon.fromJson(json)).toList();
+
+        List<Future<PokemonDetail>> futures = pokemons.map((pokemon) {
+          return fetchPokemonDetail(pokemon.url);
+        }).toList();
+
+        List<PokemonDetail> pokemonDetails = await Future.wait(futures);
+        return pokemonDetails;
+      } else {
+        throw Exception('Failed to load pokemons');
+      }
+    } catch (e) {
+      throw Exception('Failed to load Pokemon details: $e');
+    }
+  }
+
+  Future<PokemonDetail> fetchPokemonDetail(String url) async {
+    try {
+      final response = await http.get(Uri.parse(url));
+
+      if (response.statusCode == 200) {
+        final Map<String, dynamic> data = json.decode(response.body);
+        return PokemonDetail.fromJson(data);
+      } else {
+        throw Exception('Failed to load pokemon detail');
+      }
+    } catch (e) {
+      throw Exception('Failed to load Pokemon detail: $e');
+    }
   }
 
 
@@ -32,37 +70,86 @@ class PokemonInfoState extends State<PokemonInfo> {
   }
     return Scaffold(  
       backgroundColor: Colors.amber,
-      body: Container(
-      
-        margin: const EdgeInsets.all(8),
-        child: GridView.builder(
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: calculateCrossAxisCount(context), // Dynamically calculate columns
-            crossAxisSpacing: 8.0,
-            mainAxisSpacing: 8.0,
-          ),
-          itemCount: 11, // Adjust the number of items as needed
-          itemBuilder: (BuildContext context, int index) {
-            return Container(
-
-              decoration: BoxDecoration(
-              color: Colors.black,  // Set the background color
-              borderRadius: BorderRadius.circular(20.0),  // Set the border radius
-            ),
-              child: Center(
-                child: Text(
-                  'Item ${index+1}',
-                  style: TextStyle(fontSize: 18,color: Colors.white),
-                ),
-              ),
-            );
+      body: FutureBuilder<List<PokemonDetail>>(
+          future: fetchAllPokemonDetails(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return CircularProgressIndicator();
+            } else if (snapshot.hasError) {
+              return Text('Error: ${snapshot.error}');
+            } else {
+              final List<PokemonDetail> pokemons = snapshot.data!;
+              return ListView.builder(
+                itemCount: pokemons.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(pokemons[index].name),
+                  );
+                },
+              );
+            }
           },
-        ),
-      ),
+        ), 
+      
+      // Container(
+      
+      //   margin: const EdgeInsets.all(8),
+      //   child: GridView.builder(
+      //     gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+      //       crossAxisCount: calculateCrossAxisCount(context), // Dynamically calculate columns
+      //       crossAxisSpacing: 8.0,
+      //       mainAxisSpacing: 8.0,
+      //     ),
+      //     itemCount: 11, // Adjust the number of items as needed
+      //     itemBuilder: (BuildContext context, int index) {
+      //       return Container(
+
+      //         decoration: BoxDecoration(
+      //         color: Colors.black,  // Set the background color
+      //         borderRadius: BorderRadius.circular(20.0),  // Set the border radius
+      //       ),
+      //         child: Center(
+      //           child: Text(
+      //             'Item ${index+1}',
+      //             style: TextStyle(fontSize: 18,color: Colors.white),
+      //           ),
+      //         ),
+      //       );
+      //     },
+      //   ),
+      // ),
     );
   }
    
   }
+
+  class Pokemon {
+  final String name;
+  final String url;
+
+  Pokemon({required this.name, required this.url});
+
+  factory Pokemon.fromJson(Map<String, dynamic> json) {
+    return Pokemon(
+      name: json['name'],
+      url: json['url'],
+    );
+  }
+}
+
+class PokemonDetail {
+  final String name;
+  final int baseExperience;
+
+  PokemonDetail({required this.name, required this.baseExperience});
+
+  factory PokemonDetail.fromJson(Map<String, dynamic> json) {
+    return PokemonDetail(
+      name: json['name'],
+      baseExperience: json['base_experience'],
+    );
+  }
+}
 
 
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -23,7 +25,6 @@ class MyApp extends StatelessWidget {
 class Pokedex extends StatefulWidget {
   const Pokedex({super.key, required this.title});
 
-
   final String title;
 
   @override
@@ -32,44 +33,343 @@ class Pokedex extends StatefulWidget {
 
 class _PokedexState extends State<Pokedex> {
 
+  final ScrollController _scrollController = ScrollController();
+  List<PokemonDetail> allPokemom = [];
+  int offset = 0;
+  bool _isLoading = false;
+
+  bool halfwayReached = false;
+     @override
+  void initState() {
+    super.initState();
+    getPokemonDetails(0);
+    _scrollController.addListener(() {
+      if (!halfwayReached) {
+        // Calculate the halfway point
+        double halfwayPoint = (_scrollController.position.maxScrollExtent +
+                _scrollController.position.minScrollExtent) /
+            2;
+
+        // Check if the current scroll position is at the halfway point
+        if (_scrollController.position.pixels >= halfwayPoint - 50 &&
+            _scrollController.position.pixels <= halfwayPoint + 50) {
+          // Call your function when scrolled to halfway point
+         print("call");
+         offset = offset + 100;
+         if(offset <= 1000){
+           getPokemonDetails(offset);
+         }
+         
+          halfwayReached = true; // Set the flag to true
+        }
+      }
+    });
+  }
+
+   @override
+  void dispose() {
+    // Dispose of the scroll controller when the widget is disposed
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+
+   Future<List<PokemonDetail>> getPokemonDetails(int offset) async {
+    try {
+
+      setState(() {
+           _isLoading = true;        
+      });
+    
+        List<Pokemon> pokemons = await getPokemons(offset);
+
+        List<Future<PokemonDetail>> futures = pokemons.map((pokemon) {
+          return fetchPokemonDetail(pokemon.url);
+        }).toList();
+
+        List<PokemonDetail> pokemonDetails = await Future.wait(futures);
+        setState(() {
+          allPokemom.addAll(pokemonDetails);
+        }); 
+
+         setState(() {
+           _isLoading = false;        
+      });
+        return pokemonDetails;
+  
+    } catch (e) {
+         setState(() {
+           _isLoading = false;        
+      });
+      throw Exception('err Pokemon details: $e');
+    }
+  }
+
+
+  Future<List<Pokemon>> getPokemons(int offset) async {
+  List<Pokemon> pokemons = [];
+
+  try {
+ 
+      final response = await http.get(Uri.parse('https://pokeapi.co/api/v2/pokemon/?offset=$offset&limit=100'));
+
+      if (response.
+      statusCode == 200) {
+        final List<dynamic> data = json.decode(response.body)['results'];
+         pokemons = data.map((json) => Pokemon.fromJson(json)).toList();
+      } else {
+        throw Exception('Failed to load pokemons');
+      }
+
+    return pokemons;
+  } catch (e) {
+    throw Exception('Failed to load Pokemon details: $e');
+  }
+}
+
+  Future<PokemonDetail> fetchPokemonDetail(String url) async {
+    try {
+      final response = await http.get(Uri.parse(url));
+
+      if (response.statusCode == 200) {
+        final Map<String, dynamic> data = json.decode(response.body);
+        return PokemonDetail.fromJson(data);
+      } else {
+        throw Exception('Failed to load pokemon detail');
+      }
+    } catch (e) {
+      throw Exception('Failed to load Pokemon detail: $e');
+    }
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    
+
     int calculateCrossAxisCount(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
-    // Adjust the column count based on your preferences
-    int columns = (screenWidth/150).floor(); // Change 150 to your desired item width
-    return columns > 0 ? columns : 1; // Ensure at least 1 column
+    int columns = (screenWidth/150).floor(); 
+    return columns > 0 ? columns : 1; 
   }
     return Scaffold(  
       backgroundColor: Colors.amber,
-      body: Container(
+      body: 
       
-        margin: const EdgeInsets.all(8),
-        child: GridView.builder(
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: calculateCrossAxisCount(context), // Dynamically calculate columns
-            crossAxisSpacing: 8.0,
-            mainAxisSpacing: 8.0,
-          ),
-          itemCount: 11, // Adjust the number of items as needed
-          itemBuilder: (BuildContext context, int index) {
-            return Container(
+      // FutureBuilder<List<PokemonDetail>>(
+      //       future: getPokemonDetails(0),
+      //       builder: (context, snapshot) {
+      //         if (snapshot.connectionState == ConnectionState.waiting) {
+      //           return  const Center(child: CircularProgressIndicator());
+      //         } else if (snapshot.hasError) {
+      //           return Text('Error: ${snapshot.error}');
+      //         } else {
+      //           final List<PokemonDetail> pokemons = snapshot.data!;
+      //           return  GestureDetector(
+      //             onVerticalDragUpdate: (details) {
+      //               if (details.primaryDelta! < 0 && details.globalPosition.dy > 100) {
+      //                 print("at 100");
+      //               }
+      //             },
+      //             child: 
+                  ModalProgressHUD(
+                    inAsyncCall: _isLoading,
+                    child: Container(
+                      margin: EdgeInsets.all(8),
+                      child: GridView.builder(
+                              controller: _scrollController,
+                              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                                crossAxisCount: calculateCrossAxisCount(context), 
+                                crossAxisSpacing: 8.0,
+                                mainAxisSpacing: 8.0,
+                              ),
+                              itemCount: allPokemom.length, 
+                              itemBuilder: (BuildContext context, int index) {
+                                return Container(
+                                  
+                                  decoration: BoxDecoration(
+                                  color: Colors.black,  
+                                  borderRadius: BorderRadius.circular(20.0),  
+                                ),
+                                  child: Center(
+                      child:  Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Image.network("${allPokemom[index].sprites?.other?.officialArtwork?.frontDefault}",
+                            width: 100,),
+                            Text(
+                              '${allPokemom[index].name}',
+                              style: const TextStyle(fontSize: 18,color: Colors.white),
+                            ),
+                          ],
+                        ),
+                        
+                                  ),
+                                );
+                              },
+                            ),
+                    ),
+                  ),
+                );
+                
+            
+              }
+           // },
+        //  ),
+    //   );
+        
+    // }
+}
 
-              decoration: BoxDecoration(
-              color: Colors.black,  // Set the background color
-              borderRadius: BorderRadius.circular(20.0),  // Set the border radius
-            ),
-              child: Center(
-                child: Text(
-                  'Item ${index+1}',
-                  style: TextStyle(fontSize: 18,color: Colors.white),
-                ),
-              ),
-            );
-          },
-        ),
-      ),
+
+class Pokemon {
+  final String name;
+  final String url;
+
+  Pokemon({required this.name, required this.url});
+
+  factory Pokemon.fromJson(Map<String, dynamic> json) {
+    return Pokemon(
+      name: json['name'],
+      url: json['url'],
     );
+  }
+}
+
+class PokemonDetail {
+  int? height;
+  String? name;
+  int? order;
+  Sprites? sprites;
+
+  PokemonDetail({this.height, this.name, this.order, this.sprites});
+
+  PokemonDetail.fromJson(Map<String, dynamic> json) {
+    height = json['height'];
+    name = json['name'];
+    order = json['order'];
+    sprites =
+        json['sprites'] != null ? new Sprites.fromJson(json['sprites']) : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['height'] = this.height;
+    data['name'] = this.name;
+    data['order'] = this.order;
+    if (this.sprites != null) {
+      data['sprites'] = this.sprites!.toJson();
+    }
+    return data;
+  }
+}
+
+class Sprites {
+  Other? other;
+  List<Types>? types;
+  int? weight;
+
+  Sprites({this.other, this.types, this.weight});
+
+  Sprites.fromJson(Map<String, dynamic> json) {
+    other = json['other'] != null ? new Other.fromJson(json['other']) : null;
+    if (json['types'] != null) {
+      types = <Types>[];
+      json['types'].forEach((v) {
+        types!.add(new Types.fromJson(v));
+      });
+    }
+    weight = json['weight'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    if (this.other != null) {
+      data['other'] = this.other!.toJson();
+    }
+    if (this.types != null) {
+      data['types'] = this.types!.map((v) => v.toJson()).toList();
+    }
+    data['weight'] = this.weight;
+    return data;
+  }
+}
+
+class Other {
+  OfficialArtwork? officialArtwork;
+
+  Other({this.officialArtwork});
+
+  Other.fromJson(Map<String, dynamic> json) {
+    officialArtwork = json['official-artwork'] != null
+        ? new OfficialArtwork.fromJson(json['official-artwork'])
+        : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    if (this.officialArtwork != null) {
+      data['official-artwork'] = this.officialArtwork!.toJson();
+    }
+    return data;
+  }
+}
+
+class OfficialArtwork {
+  String? frontDefault;
+  String? frontShiny;
+
+  OfficialArtwork({this.frontDefault, this.frontShiny});
+
+  OfficialArtwork.fromJson(Map<String, dynamic> json) {
+    frontDefault = json['front_default'];
+    frontShiny = json['front_shiny'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['front_default'] = this.frontDefault;
+    data['front_shiny'] = this.frontShiny;
+    return data;
+  }
+}
+
+class Types {
+  int? slot;
+  Type? type;
+
+  Types({this.slot, this.type});
+
+  Types.fromJson(Map<String, dynamic> json) {
+    slot = json['slot'];
+    type = json['type'] != null ? new Type.fromJson(json['type']) : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['slot'] = this.slot;
+    if (this.type != null) {
+      data['type'] = this.type!.toJson();
+    }
+    return data;
+  }
+}
+
+class Type {
+  String? name;
+  String? url;
+
+  Type({this.name, this.url});
+
+  Type.fromJson(Map<String, dynamic> json) {
+    name = json['name'];
+    url = json['url'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['name'] = this.name;
+    data['url'] = this.url;
+    return data;
   }
 }
